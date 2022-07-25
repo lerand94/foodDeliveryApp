e8b73a2398bc0c8972608061ab2a2411d30809c3
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-25 19:02:46 +0300

    created Modal cmp like as Portal for clear code and good practices

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <div>
+      <Cart />
       <Header />
-      <Meals />
+      <main>
+        <Meals />
+      </main>
     </div>
   );
 }
