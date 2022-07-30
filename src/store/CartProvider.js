31cdb19fc95b7e70ff7f8f3cd0e66e3977415029
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-30 20:41:34 +0300

    some fix our UX after submitting order. User got feedback after submitting

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,7 +2,10 @@ import { useReducer } from "react";
 
 import CartContext from "./cart-context";
 
-const defaultCartState = { items: [], totalAmount: 0 };
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
@@ -12,9 +15,7 @@ const cartReducer = (state, action) => {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
-
     const existingCartItem = state.items[existingCartItemIndex];
-
     let updatedItems;
 
     if (existingCartItem) {
@@ -28,9 +29,11 @@ const cartReducer = (state, action) => {
       updatedItems = state.items.concat(action.item);
     }
 
-    return { items: updatedItems, totalAmount: updatedTotalAmount };
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
-
   if (action.type === "REMOVE") {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
@@ -52,6 +55,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -69,11 +76,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
