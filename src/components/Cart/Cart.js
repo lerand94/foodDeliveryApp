b9223c83cd07fe1329265b2f0153857ea37c8de4
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-30 20:35:58 +0300

    realised submitting our order to backend API

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -25,6 +25,20 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch(
+      "https://fooddelivery-ef190-default-rtdb.firebaseio.com\n" +
+        "\n/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -60,7 +74,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
